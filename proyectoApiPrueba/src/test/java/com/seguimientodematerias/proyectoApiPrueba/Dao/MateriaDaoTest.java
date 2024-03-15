896bbe9557eb3cd32ac40c1edac3baffb33177bc
Juan-Manuel-Prividera <jmprividera@gmail.com>
2024-03-15 13:51:57 -0300

    Algunos test

--- proyectoApiPrueba/src/test/java/com/seguimientodematerias/proyectoApiPrueba/Dao/MateriaDaoTest.java
+++ proyectoApiPrueba/src/test/java/com/seguimientodematerias/proyectoApiPrueba/Dao/MateriaDaoTest.java
@@ -1,4 +1,53 @@
 package com.seguimientodematerias.proyectoApiPrueba.Dao;
 
+import com.seguimientodematerias.proyectoApiPrueba.models.Materia;
+import jakarta.persistence.EntityManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 public class MateriaDaoTest {
+
+    @InjectMocks
+    private MateriaDao materiaDao;
+
+    @Mock
+    private EntityManager entityManager;
+
+    private Materia materia;
+
+    @Before
+    public void setUp() {
+        materia = new Materia();
+        materia.setNombre("Hola");
+        materia.setAnioMateria("1");
+        materia.setId(99L);
+        materia.setAnioCursada("2030");
+        materia.setPromocion(false);
+        materia.setNotaFinal(10);
+        materia.setNotaCursada(7);
+    }
+    @Test
+    public void getMateriasTest(){
+        List<Materia> materiaList = new ArrayList<>();
+        materiaList.add(materia);
+        String query = "FROM Materia WHERE anioMateria = :anioMateria";
+
+        when (entityManager.createQuery(query, Materia.class).setParameter("anioMateria", "1").getResultList()).thenReturn(materiaList);
+
+        List<Materia> resultado = materiaDao.getMaterias("1");
+
+        Assert.assertEquals(materiaList,resultado);
+    }
 }
--- proyectoApiPrueba/src/test/java/com/seguimientodematerias/proyectoApiPrueba/service/MateriaServiceTest.java
+++ proyectoApiPrueba/src/test/java/com/seguimientodematerias/proyectoApiPrueba/service/MateriaServiceTest.java
@@ -1,4 +1,59 @@
 package com.seguimientodematerias.proyectoApiPrueba.service;
 
+import com.seguimientodematerias.proyectoApiPrueba.Dao.MateriaDao;
+import com.seguimientodematerias.proyectoApiPrueba.models.Materia;
+import com.seguimientodematerias.proyectoApiPrueba.services.MateriaService;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
 public class MateriaServiceTest {
+    @InjectMocks
+    private MateriaService service;
+    @Mock
+    private MateriaDao materiaDao;
+    private Materia materia;
+    public void setUp() {
+        materia = new Materia();
+        materia.setNombre("Hola");
+        materia.setAnioMateria("1");
+        materia.setId(99L);
+        materia.setAnioCursada("2030");
+        materia.setPromocion(false);
+        materia.setNotaFinal(10);
+        materia.setNotaCursada(7);
+    }
+    @Test
+    public void getMateriasTest(){
+        String anioMateria = "1";
+        List<Materia> materiaList = new ArrayList<>();
+        materiaList.add(materia);
+
+        when(materiaDao.getMaterias(anioMateria)).thenReturn(materiaList);
+        List<Materia> resultado = service.getMaterias(anioMateria);
+
+        Assertions.assertEquals(resultado, materiaList);
+        verify(materiaDao,times(1)).getMaterias(anioMateria);
+    }
+
+    @Test
+    public void modificarMateriasTest(){
+        service.modificarMateria(materia);
+        verify(materiaDao,times(1)).modificarMateria(materia);
+    }
+
+
+
 }
