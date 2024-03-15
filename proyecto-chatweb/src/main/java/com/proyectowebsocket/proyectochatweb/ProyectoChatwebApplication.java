3d1c420fd04b92c534ec77d9f803a14775c39b2a
Juan-Manuel-Prividera <jmprividera@gmail.com>
2024-03-15 17:30:05 -0300

    Creando proyecto chat-web

---
+++ proyecto-chatweb/src/main/java/com/proyectowebsocket/proyectochatweb/ProyectoChatwebApplication.java
@@ -0,0 +1,13 @@
+package com.proyectowebsocket.proyectochatweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoChatwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoChatwebApplication.class, args);
+	}
+
+}
---
+++ proyecto-chatweb/src/test/java/com/proyectowebsocket/proyectochatweb/ProyectoChatwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectowebsocket.proyectochatweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoChatwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
